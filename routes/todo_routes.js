bfccf651ed21f9b94b279972727b9006c5e4af97
Eunice <omgunis@gmail.com>
2015-11-17 15:52:43 -0800

    Added index.html to root route

--- routes/todo_routes.js
+++ routes/todo_routes.js
@@ -3,12 +3,6 @@ var express        = require('express');
 var todoRoutes     = express.Router();
 
 todoRoutes.route('/api/todos')
-  .get(todoController.index)
-  .post(todoController.create);
-
-todoRoutes.route('/:text')
-  .get(todoController.show)
-  .patch(todoController.update)
-  .delete(todoController.destroy);
+  .get(todoController.index);
 
 module.exports = todoRoutes;
