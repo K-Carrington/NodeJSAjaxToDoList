9adbbff3a4aa3c8e018aa9c3fa6075ed7166aaff
nickhendren <nhendren7@gmail.com>
2015-11-17 14:17:53 -0800

    finished the controller and routes

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -7,6 +7,42 @@ function index(req, res) {
   });
 };
 
+function create(req, res) {
+  var todo = new Todo();
+  todo.text = req.body.text;
+  todo.boolean = req.body.boolean;
+
+  todo.save(function(err) {
+    if (err) throw err;
+    res.json({success: true, message: "Todo list created!"});
+  });
+};
+
+function show(req, res) {
+  Todo.find({text: req.params.text}, function(err, todo) {
+    if (err) throw err;
+    res.json(todo);
+  });
+};
+
+function update(req, res) {
+  Todo.findOneAndUpdate({text: req.body.text}, function(err, todo) {
+    if (err) throw err;
+    res.json(todo);
+  });
+};
+
+function destroy(req, res) {
+  Todo.remove({text: req.params.text}, function(err) {
+    if (err) throw err;
+    res.json({success: true, message: "Todo list deleted!"});
+  });
+};
+
 module.exports = {
-  index:index
+  index:index,
+  show: show,
+  create: create,
+  update: update,
+  destroy: destroy
 };
