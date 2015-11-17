190bd855b6beb45b725eb0c92101a7c441c8eecf
nickhendren <nhendren7@gmail.com>
2015-11-17 12:31:03 -0800

    changed typo

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,7 +1,7 @@
 var Todo = require ('../models/todo.js');
 
 function index(req, res) {
-  To_do.find({}, function(err, todo) {
+  Todo.find({}, function(err, todo) {
     if (err) throw err;
     res.json(todo);
   });
