af301c7b820cc03d46869aae0a424b4c6f139b54
Ken Carrington <kcarring93@verizon.net>
2015-11-17 16:20:16 -0800

    fixed more of old ajax branch merge conflicts

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ var Todo       = require('./models/todo.js');
 //connect to mongo db
 mongoose.connect('mongodb://localhost/todosdb');
 
-<<<<<<< HEAD
-=======
 //create 1 Todo item in db
->>>>>>> 377055b048612119c80da16cc798d296de50441b
 var todo1 = new Todo({
   text: "Milk",
 	done: true
@@ -25,14 +22,11 @@ todo1.save(function(err){
   console.log("To do item added");
 });
 
-<<<<<<< HEAD
 app.get('/api/todos', todoRoutes);
-=======
 app.use('/', todoRoutes);
 
 //Static page, root route shows index.html
 app.use(express.static('public'))
->>>>>>> 377055b048612119c80da16cc798d296de50441b
 
 app.listen(port, function(){
   console.log('Listening on port ' + port);
